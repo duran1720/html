2365efcfad777c88a454a0bc35f885dda386f3ec
Stiven Duran <duranb1720@gmail.com>
2025-05-05 12:21:05 -0500

    primeros ejercicios js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let opc=parseInt( prompt("que operacion quieres hacer  1.suma 2.resta 3..multiplicación : "));
+
+if (opc==1){
+    document.writeln("La suma de " + numero1 + " + " + numero2 + " es igual a : " + (numero1+numero2) + "<br>");      
+}
+if (opc==2){
+    document.writeln("La resta de " + numero1 + " - " + numero2 + " es igual a : " + (numero1-numero2) + "<br>");      
+}
+if (opc==3){
+    document.writeln("La multiplicacion de " + numero1 + " * " + numero2 + " es igual a : " + (numero1*numero2) + "<br>");      
+}   
---
+++ js/eje1.js
@@ -0,0 +1,4 @@
+let x=3
+let y=4.5
+let z=x*y
+console.log(z)
